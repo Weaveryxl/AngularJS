1da7483bae24a179d6e1f2da93b3112211b663b8
Weaveryxl <weaveryxl549@gmail.com>
2017-05-19 11:38:19 -0400

    Moduel, Controller, ng-src, TwoWayDatabinding

---
+++ AngularJSPractise/scripts/Script.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.js" />
+var myApp = angular
+                    .module("myModule", [])
+                    .controller("myController", function ($scope) {
+                        var employee = {
+                            firstName: "David",
+                            lastName: "Hastings",
+                            gender: "Male",
+                            picture: "/Image/1436278541_7872.jpg"
+                        };
+                        $scope.employee = employee;
+                        $scope.message = "Hello Angular!";
+                    });
